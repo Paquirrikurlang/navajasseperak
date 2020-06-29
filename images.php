82e4bc9047674712ec7cf6b2ee7c2841fa05c4bf
Paquirri <spaquirri@gmail.com>
2020-06-28 21:53:41 -0500

    Imágenes

--- images.php
+++ images.php
@@ -83,51 +83,7 @@
                                                 <div class="col-md-12">
                                                     <div id="mdb-lightbox-ui"></div>
                                                     <div class="mdb-lightbox" id="images" data-pswp-uid="1">
-                                                        <figure class="col-md-4">
-                                                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(145).jpg" data-size="1600x1067">
-                                                                <img alt="picture" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(145).jpg" class="img-fluid">
-                                                            </a>
-                                                        </figure>
-                                                        <figure class="col-md-4">
-                                                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(150).jpg" data-size="1600x1067">
-                                                                <img alt="picture" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(150).jpg" class="img-fluid">
-                                                            </a>
-                                                        </figure>
-                                                        <figure class="col-md-4">
-                                                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(152).jpg" data-size="1600x1067">
-                                                                <img alt="picture" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(152).jpg" class="img-fluid">
-                                                            </a>
-                                                        </figure>
-                                                        <figure class="col-md-4">
-                                                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(42).jpg" data-size="1600x1067">
-                                                                <img alt="picture" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(42).jpg" class="img-fluid">
-                                                            </a>
-                                                        </figure>
-                                                        <figure class="col-md-4">
-                                                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(151).jpg" data-size="1600x1067">
-                                                                <img alt="picture" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(151).jpg" class="img-fluid">
-                                                            </a>
-                                                        </figure>
-                                                        <figure class="col-md-4">
-                                                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(40).jpg" data-size="1600x1067">
-                                                                <img alt="picture" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(40).jpg" class="img-fluid">
-                                                            </a>
-                                                        </figure>
-                                                        <figure class="col-md-4">
-                                                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(148).jpg" data-size="1600x1067">
-                                                                <img alt="picture" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(148).jpg" class="img-fluid">
-                                                            </a>
-                                                        </figure>
-                                                        <figure class="col-md-4">
-                                                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(147).jpg" data-size="1600x1067">
-                                                                <img alt="picture" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg" class="img-fluid">
-                                                            </a>
-                                                        </figure>
-                                                        <figure class="col-md-4">
-                                                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(149).jpg" data-size="1600x1067">
-                                                                <img alt="picture" src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(149).jpg" class="img-fluid">
-                                                            </a>
-                                                        </figure>
+
                                                     </div>
                                                 </div>
                                             </div>
@@ -140,12 +96,28 @@
                                 </section>
                             </div>
                         </div>
+                        <div class="products-view__pagination">
+                            <ul class="pagination justify-content-center">
+                                <li class="page-item disabled"><a class="page-link page-link--with-arrow" href="" aria-label="Previous"><svg class="page-link__arrow page-link__arrow--left" aria-hidden="true" width="8px" height="13px">
+                                            <use xlink:href="images/sprite.svg#arrow-rounded-left-8x13"></use>
+                                        </svg></a></li>
+                                <li class="page-item"><a class="page-link" href="">1</a></li>
+                                <li class="page-item active"><a class="page-link" href="">2 <span class="sr-only">(current)</span></a></li>
+                                <li class="page-item"><a class="page-link" href="">3</a></li>
+                                <li class="page-item"><a class="page-link page-link--with-arrow" href="" aria-label="Next"><svg class="page-link__arrow page-link__arrow--right" aria-hidden="true" width="8px" height="13px">
+                                            <use xlink:href="images/sprite.svg#arrow-rounded-right-8x13"></use>
+                                        </svg></a></li>
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </main>
+    <?php include_once('mvc/view/layouts/footer.php') ?>
     <script type="text/javascript" src="https://z9t4u9f6.stackpathcdn.com/wp-content/themes/mdbootstrap4/js/bundles/4.19.1/compiled.16.min.js?ver=4.19.1"></script>
+    <script src="js/LoadImagenes.js"></script>
 </body>
 
 </html>
--- js/LoadImagenes.js
+++ js/LoadImagenes.js
@@ -3,12 +3,12 @@
   $(function () {
     debugger;
     var params = new URLSearchParams(location.search);
-    var TipoMultimedia = params.get("TipoMultimedia");
-    LoadImages(TipoMultimedia);
+    var IdGaleria = params.get("IdGaleria");
+    LoadImages(IdGaleria);
   });
 })(jQuery);
 
-function LoadImages(TipoMultimedia) {
+function LoadImages(IdGaleria) {
   /* 1 -> Videos, 2 ->Fotos */
   var resultadoMultimedia = JSON.parse(
     localStorage.getItem("resultadoMultimedia")
@@ -18,5 +18,17 @@ function LoadImages(TipoMultimedia) {
     (data) => data.GaleriaId == IdGaleria
   );
 
-  jQuery.each(resultadoContenido, function (i, val) {});
+  jQuery.each(resultadoContenido, function (i, val) {
+    $("#images").append(
+      "<figure class='col-md-4'>" +
+        "<a href='" +
+        val.RutaImagen +
+        "' data-size='1600x1067'>" +
+        "<img alt='picture' src='" +
+        val.RutaImagen +
+        "' class='img-fluid'>" +
+        "</a>" +
+        "</figure>"
+    );
+  });
 }
